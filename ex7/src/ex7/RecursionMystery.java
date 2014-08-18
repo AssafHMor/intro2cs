cd700386de0049ada89dda7bc8f245f68f914630
AssafHMor <rammenn@gmail.com>
2014-08-18 11:39:52 +0300

    intro2cs exercises

---
+++ ex7/src/ex7/RecursionMystery.java
@@ -0,0 +1,33 @@
+package ex7;
+/**
+ * description: a class that implements two methods:
+ * the first calculate Fibonacci sequence
+ * and one who converts numbers to binary base
+ */
+public class RecursionMystery {
+	
+	/**
+	 * this method returns the n'th number in Fibonacci sequence
+	 * @param n the index of number in Fibonacci sequence
+	 * @return the number at the n'th place in Fibonacci sequence
+	 */
+    public static int mystery1(int n){
+    	if(n<=1)
+    		return n;
+    	return mystery1(n-1) + mystery1(n-2);
+    }
+    
+    /**
+     * this method creates an array of integers presenting the binary value of N
+     * @param N the number to convert to binary
+     * @param res the array for output
+     */
+    public static void mystery2(int N, int[] res){
+    	
+    	res[(int)(Math.log10(N)/Math.log10(2))] = N%2;
+    	if(N<=1) 
+    		return;
+    	mystery2(N/2, res);
+    }
+    
+}
---
+++ ex8/src/ex8/RealFunction.java
@@ -0,0 +1,21 @@
+package ex8;
+/**
+ * description: A interface defined especially for ex8.
+ * An interface that represents classes that are real valued functions.
+ */
+public interface RealFunction {
+	
+	/**
+	 * returns the f(x) value of the RealFunction.
+	 * @param x the x value given.
+	 * @return the f(x) value of this function.
+	 */
+	public double valueAt(double x);
+	
+	/**
+	 * returns a String representation of the function. 
+	 * @return a String representation of the function. 
+	 */
+	@Override
+	public String toString();
+}
